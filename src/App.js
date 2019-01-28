8f6a299d5e94fe860edea942c417a82c0c1773e9
Austin Roy <austin.roy@andela.com>
2019-01-28 14:57:26 +0300

    Add ability to handle button inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MainForm from './components/MainForm';
 import { Container } from 'semantic-ui-react';
